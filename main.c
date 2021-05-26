6e47a74a174ab6ba1373f50a2b930e183f2f1459
sandeep123khadka <sandeep456khadka@gmail.com>
2021-05-26 12:17:38 +0545

    my first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+int main() {
+    int a;
+    int b;
+    printf("enter the first number");
+    scanf("%d",&a);
+    printf("enter the second number");
+    scanf("%d",&b);
+    int c;
+    c=a+b;
+    printf("the sum is: %d",c);
+int x;
+x=a*b;
+printf("\nthe product is: %d",x);
+ return 0;
+}
